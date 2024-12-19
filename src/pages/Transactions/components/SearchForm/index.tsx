625d044b07d2fc32b27ba00f9db7491d00175529
JonasMaia12 <jonaspiresmaia7@gmail.com>
2024-12-19 11:06:03 +0000

    Add performance

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,8 +3,8 @@ import { SearchFormContainer } from "./styles"
 import { useForm } from "react-hook-form"
 import * as z from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { useContext } from "react"
 import { TransactionContext } from "../../../../contexts/TransactionsContext"
+import { useContextSelector } from "use-context-selector"
 
 const SearchFormSchema = z.object({
   query: z.string().min(3),
@@ -13,7 +13,12 @@ const SearchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof SearchFormSchema>
 
 export function SearchForm() {
-  const { fetchTransactions } = useContext(TransactionContext)
+  const fetchTransactions = useContextSelector(
+    TransactionContext,
+    (context) => {
+      return context.fetchTransactions
+    }
+  )
 
   const {
     register,
